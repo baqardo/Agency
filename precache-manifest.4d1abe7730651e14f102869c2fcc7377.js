51d1084a60368632e18dd12c4b7e1f32ee3a88ca
Andrzej Nawalaniec <46224560+baqardo@users.noreply.github.com>
2021-05-13 15:43:11 +0200

    Updates

---
+++ precache-manifest.4d1abe7730651e14f102869c2fcc7377.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722c0ee82c133f8d1400c54689061b16",
+    "url": "/Agency/index.html"
+  },
+  {
+    "revision": "9f8c95c2a1085211f97b",
+    "url": "/Agency/static/css/main.f5043432.chunk.css"
+  },
+  {
+    "revision": "044cda9f943b2e23f68d",
+    "url": "/Agency/static/js/2.dd920f41.chunk.js"
+  },
+  {
+    "revision": "08766c0f007212bccedd532544202895",
+    "url": "/Agency/static/js/2.dd920f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f8c95c2a1085211f97b",
+    "url": "/Agency/static/js/main.5ff4b405.chunk.js"
+  },
+  {
+    "revision": "f54fb67bfaa7396c68a6",
+    "url": "/Agency/static/js/runtime-main.586c99bd.js"
+  },
+  {
+    "revision": "944df993a140295c21e62b27546462d2",
+    "url": "/Agency/static/media/LemonMilk.944df993.woff"
+  },
+  {
+    "revision": "9d3ae664766c5e04d2523cc193b7a7ce",
+    "url": "/Agency/static/media/LemonMilkBold.9d3ae664.woff"
+  },
+  {
+    "revision": "bb255cd0eddfad3e65855d0128dce38e",
+    "url": "/Agency/static/media/LemonMilkBold.bb255cd0.otf"
+  },
+  {
+    "revision": "d76b6b42192a241fceb43f9cab4f38b4",
+    "url": "/Agency/static/media/LemonMilkLight.d76b6b42.woff"
+  },
+  {
+    "revision": "aeba9f64b595a13abc7af2d1c4314a1a",
+    "url": "/Agency/static/media/PeaceSans.aeba9f64.otf"
+  },
+  {
+    "revision": "883179f33ee65d17ad14b40854821539",
+    "url": "/Agency/static/media/city-image.883179f3.png"
+  },
+  {
+    "revision": "4d9acd630326851e830cd9425b0c96ad",
+    "url": "/Agency/static/media/design.4d9acd63.png"
+  },
+  {
+    "revision": "ead36fc415824decd302b646b3cccf2a",
+    "url": "/Agency/static/media/footer-background.ead36fc4.png"
+  },
+  {
+    "revision": "5a3e0a6fb9800a06d33d04c5376044c0",
+    "url": "/Agency/static/media/idea.5a3e0a6f.png"
+  },
+  {
+    "revision": "8b1bf01861a067bbd21e70cddf0802bf",
+    "url": "/Agency/static/media/macbook.8b1bf018.png"
+  },
+  {
+    "revision": "105688539590002919183c280fd5f205",
+    "url": "/Agency/static/media/model-image.10568853.png"
+  },
+  {
+    "revision": "bc9444da29d2d9c02ac404959a10feff",
+    "url": "/Agency/static/media/model_shade.bc9444da.png"
+  },
+  {
+    "revision": "d33daca2dd77fe512566ddf2aced818a",
+    "url": "/Agency/static/media/output.d33daca2.png"
+  },
+  {
+    "revision": "566e78b31e4e6f6b6fda17adf682e723",
+    "url": "/Agency/static/media/slider_image1.566e78b3.png"
+  },
+  {
+    "revision": "0bf7b301681cbb3ee79cb764813a0732",
+    "url": "/Agency/static/media/slider_image2.0bf7b301.png"
+  },
+  {
+    "revision": "420044ae3b2825b8d930ec21a0714c95",
+    "url": "/Agency/static/media/slider_image3.420044ae.png"
+  },
+  {
+    "revision": "9969a4860ed2d3aa14297a1126abf205",
+    "url": "/Agency/static/media/small-image1.9969a486.png"
+  },
+  {
+    "revision": "eaaa04d4fbbd9bbbcafc2270e95c563d",
+    "url": "/Agency/static/media/small-image2.eaaa04d4.png"
+  },
+  {
+    "revision": "d2ca01e80006364ecfab998f1a7a2e13",
+    "url": "/Agency/static/media/small-image3.d2ca01e8.png"
+  },
+  {
+    "revision": "0bf7b301681cbb3ee79cb764813a0732",
+    "url": "/Agency/static/media/testimonials_image1.0bf7b301.png"
+  },
+  {
+    "revision": "566e78b31e4e6f6b6fda17adf682e723",
+    "url": "/Agency/static/media/testimonials_image2.566e78b3.png"
+  },
+  {
+    "revision": "d4030d94c726b1dfb2fa65aae69bfe96",
+    "url": "/Agency/static/media/testimonials_image3.d4030d94.png"
+  }
+]);
